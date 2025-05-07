1cb31f943b488fe47d30474374a1bc32afb7942b
juanjosee15 <juaperezv@upt.pe>
2025-05-06 23:18:12 -0500

    login actualizado

---
+++ proyecto_peti/Models/Mision.cs
@@ -0,0 +1,20 @@
+namespace proyecto_peti.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Mision")]
+    public partial class Mision
+    {
+        public int Id { get; set; }
+
+        public int PlanId { get; set; }
+
+        public string Contenido { get; set; }
+
+        public virtual PlanEstrategico PlanEstrategico { get; set; }
+    }
+}
