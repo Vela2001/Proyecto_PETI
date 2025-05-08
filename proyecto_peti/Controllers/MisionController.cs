e646dd8e5924da347916de189263a6a13f51b734
Vela2001 <abrvelav@upt.pe>
2025-05-08 00:04:22 -0500

    Vela_original

---
+++ proyecto_peti/Controllers/MisionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proyecto_peti.Controllers
+{
+    public class MisionController : Controller
+    {
+        // GET: Mision
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
